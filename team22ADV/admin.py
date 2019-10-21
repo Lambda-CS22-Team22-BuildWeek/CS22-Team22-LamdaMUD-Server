6c584596a47fcd7304f4555dad32e5f2402647fe
mparedes003 <onnabugeishadev@gmail.com>
2019-10-21 13:33:12 -0700

    Register models with admin site

--- team22ADV/admin.py
+++ team22ADV/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Room, Player
 
 # Register your models here.
+admin.site.register(Room) # register the Room model with the admin site 
+admin.site.register(Player) # register the Player model with the admin site 
--- team22ADV/models.py
+++ team22ADV/models.py
@@ -41,6 +41,8 @@ class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     currentRoom = models.IntegerField(default=0)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True) # track created dates, auto_now_add only sets create
+    last_modified = models.DateTimeField(auto_now=True) # track modified dates, auto_now sets both create and update
     def initialize(self):
         if self.currentRoom == 0:
             self.currentRoom = Room.objects.first().id
